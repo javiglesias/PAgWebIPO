63f94b788095a5761042407caea9a9db380d4661
javiglesias <javiglesias18@gmail.com>
2015-03-13 21:40:29 +0100

    Limpiar fotos de Contacto

--- actualizarDesign.js
+++ actualizarDesign.js
@@ -1,4 +1,15 @@
-function cambioCSS(css_title)
+function cambioCSS1()
 {
-
+    var x = document.getElementById("disenio");
+    x.href="disenio1.css";
+}
+function cambioCSS2()
+{
+    var x = document.getElementById("disenio");
+    x.href="disenio2.css";
+}
+function cambioCSS3()
+{
+    var x = document.getElementById("disenio");
+    x.href="disenio3.css";
 }
